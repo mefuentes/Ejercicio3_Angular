3078c03f93de6890831b908d60279b7161557a2b
Mariano Fuentes <marianofu10@gmail.com>
2024-11-20 16:10:03 -0300

    Ejerc3_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejercicio3';
+  nombre: string = "Mariano";
 }
